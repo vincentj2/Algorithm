24be7d1878d665339a8d44749b3a1e311984da3b
vincentj2 <vincentj@naver.com>
2020-08-30 23:16:43 +0900

    [programmers 고득점kit] [해시]: 완주하지 못한 선수

---
+++ Programmers/완주하지 못한 선수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i =0; i<participant.length; i++){
+            if(!map.containsKey(participant[i]) ){
+                map.put(participant[i],1);
+            }else{
+                map.put(participant[i], map.get(participant[i])+1);
+            }
+        }
+
+        for(int j =0;j<completion.length; j++){
+            map.put(completion[j],map.get(completion[j])-1);
+        }
+        for(String anw : map.keySet()){
+            if(map.get(anw) == 1){
+                answer = anw;
+            }
+        }
+        //  answer = map.getKey(1);
+        return answer;
+    }
+}
