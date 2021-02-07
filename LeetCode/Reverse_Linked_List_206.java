00f80562ed0f99392a549da94bbc7518b9efefd4
vincentj2 <vincentj@naver.com>
2021-02-07 14:28:19 +0900

    [LeetCode] : 206 Reverse Linked List add note

--- LeetCode/Reverse_Linked_List_206.java
+++ LeetCode/Reverse_Linked_List_206.java
@@ -7,7 +7,12 @@ package Algorthm.LeetCode;
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  }
-
+/*
+핵심 알고리즘 : linked list
+고민거리 : iteration 과 recursive 모두 사용하여 풀이가 가능하다
+          null 값을 갖고 있는 노드를 사용하여 역순 노드의 마지막이 되도록 지정하고
+          순차적으로 순회하며 역순으로 배치한다.
+ */
 
 // iteration 풀이
 class Solution {
