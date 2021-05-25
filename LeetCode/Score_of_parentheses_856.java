c2b1186594a71ecd57a33ab810ee42b1670407ae
vincentj2 <vincentj@naver.com>
2021-05-25 13:52:08 +0900

    856

--- LeetCode/Score_of_parentheses_856.java
+++ LeetCode/Score_of_parentheses_856.java
@@ -1,16 +1,18 @@
-public int scoreOfParentheses(String S) {
+class Solution {
+    public int scoreOfParentheses(String s) {
         Stack<Integer> stack = new Stack();
         stack.push(0); // The score of the current frame
 
-        for (char c: S.toCharArray()) {
-        if (c == '(')
-        stack.push(0);
-        else {
-        int v = stack.pop();
-        int w = stack.pop();
-        stack.push(w + Math.max(2 * v, 1));
-        }
+        for (char c: s.toCharArray()) {
+            if (c == '(')
+                stack.push(0);
+            else {
+                int v = stack.pop();
+                int w = stack.pop();
+                stack.push(w + Math.max(2 * v, 1));
+            }
         }
 
         return stack.pop();
-        }
+    }
+}
