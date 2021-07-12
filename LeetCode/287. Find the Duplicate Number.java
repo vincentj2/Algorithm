e34b5ab4c3cff261ccc74e26b097953fa1ce2b0d
vincentj2 <vincentj@naver.com>
2021-07-12 11:57:06 +0900

    287. Find the Duplicate Number

---
+++ LeetCode/287. Find the Duplicate Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow =0;
+        int fast =0;
+
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if(slow == fast) break;
+        }
+
+        slow =0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[fast];
+
+            if(slow == fast){
+                return slow;
+            }
+
+        }
+    }
+}
